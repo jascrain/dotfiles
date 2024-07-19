fd8147797fa89ef74545bb7b578162a124c5f490
Jason Crain <jason@inspiresomeone.us>
2024-07-19 10:03:14 -0600

    2024-07-19

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -4,25 +4,27 @@ return {
     opts = {
         plugins = { spelling = true },
         defaults = {
-            mode = { "n", "v" },
-            ["g"] = { name = "+goto" },
-            ["gd"] = { name = "Definition" },
-            ["gD"] = { name = "Declaration" },
-            ["z"] = { name = "+fold" },
-            ["K"] = { name = "Keyword Lookup" },
-            ["]"] = { name = "+next" },
-            ["["] = { name = "+prev" },
-            ["<leader>c"] = { name = "+code" },
-            ["<leader>f"] = { name = "+find" },
-            ["<leader>g"] = { name = "+git" },
-            ["<leader>gh"] = { name = "+hunks" },
-            ["<leader>s"] = { name = "+search" },
-            ["<leader>u"] = { name = "+ui" },
+            {
+                mode = { "n", "v" },
+                { "<leader>c", group = "code" },
+                { "<leader>f", group = "find" },
+                { "<leader>g", group = "git" },
+                { "<leader>h", group = "hunks" },
+                { "<leader>s", group = "search" },
+                { "<leader>u", group = "ui" },
+                { "K", desc = "Keyword Lookup" },
+                { "[", group = "prev" },
+                { "]", group = "next" },
+                { "g", group = "goto" },
+                { "gD", desc = "Declaration" },
+                { "gd", desc = "Definition" },
+                { "z", group = "fold" },
+            },
         },
     },
     config = function(_, opts)
         local wk = require("which-key")
         wk.setup(opts)
-        wk.register(opts.defaults)
+        wk.add(opts.defaults)
     end,
 }
